6fe7e31e3c1987cc2ff35e8b27c787b5bab64264
Mateus <mateusnobref@gmail.com>
2019-01-09 15:51:39 -0300

    CCOfigurado responsividade header. Adicionado alguns campos no formulário de adição e corrigido alguns bugs

--- src/Livro.js
+++ src/Livro.js
@@ -4,12 +4,16 @@ export default class Livro{
     authors;
     description;
     imgUrl;
+    editora;
+    quantidade;
 
-    constructor(isbn, title, authors, description, imgUrl){
+    constructor(isbn, title, authors, description, imgUrl, editora, quantidade){
         this.isbn = isbn;
         this.title=title;
         this.authors=authors;
         this.description=description;
-        this.imgUrl = imgUrl
+        this.imgUrl = imgUrl;
+        this.editora = editora;
+        this.quantidade = quantidade;
     }
 }
